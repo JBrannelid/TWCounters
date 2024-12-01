ffda497c5d6762f501466cf009ec17dad2c0b3ac
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2024-12-01 23:32:53 +0100

    Rebuild the project and push

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: "/TWCounters/", // Uppdatera med ditt repository-namn
   plugins: [react()],
   resolve: {
     alias: [
